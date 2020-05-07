cf96decedce98806eafd0b10f7143891ec3ef2b3
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-05-08 00:17:52 +0200

    Create version.php

---
+++ app/version.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *  Project: KanFF
+ *  File: version.php file about indications of the versions.
+ *  Author: Team
+ *  Creation date: 07.05.2020
+ */
+
+//Version of the app:
+$version = 1.0;
+//Date of the publication of the version
+$dateversion = "2020-05-08";
+
+$changelog = [
+	"1.0": ""
+];
+
+?>
