1a759310797bd221234e5f355269eda2100cd4bc
Miguel Soares <47849626+miguelsoaresking500@users.noreply.github.com>
2020-05-08 09:12:13 +0200

    log in mise ne page et signin

--- app/view/Login.php
+++ app/view/Login.php
@@ -4,19 +4,24 @@ ob_start();
 
 ?>
 
-    <div class="container">
-        <h1>Login</h1>
-        <form action="index.php?action=tryLogIn" method="post">
-            <a>Connexion</a>
-            Utilisateur ou Email <input type="text" name="user" placeholder="Username or email"  required="required" />
-            <br>
-            Mot de passe <input type="password" name="password" placeholder="Password" required="required" />
-            <br>
-            <button type="submit" class="btn btn-primary btn-block btn-large">Login</button>
-            <p>Pas encore de compte sur cette instance Blason?</p>
-            <p>Créez un copte et rejoignez ensuite un collectif!</p>
-        </form>
-    </div>
+    <form class="formlog" action="index.php?action=tryLogIn" method="post">
+        <div>
+        <a>Bienvenue sur l'instance Blason. L'instance KanFF de GdC pour toute la Romandie.</a>
+            <img src="medias/logos/vuLogoLogin.png" alt="" width="301" height="167">
+        </div>
+        <div class="form-group" action="index.php?action=tryLogIn" method="post">
+            <label for="exampleInputEmail1">Utilisateur ou Email :</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter or Username">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1"> Mot de passe : </label>
+           <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary btn-block ">Login</button>
+        <p>Pas encore de compte sur cette instance Blason?</p>
+        <a href="index.php?action=displaySignin"><p>Créez un copte et rejoignez ensuite un collectif!</p></a>
+    </form>
+
 <?php
 $content = ob_get_clean();
 require "gabarit.php";
