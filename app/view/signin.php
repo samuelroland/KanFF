0bbdc0edd608ce537be6e1bc6117636f533838c8
Miguel Soares <47849626+miguelsoaresking500@users.noreply.github.com>
2020-05-18 14:45:04 +0200

    login changements

--- app/view/signin.php
+++ app/view/signin.php
@@ -3,61 +3,82 @@ ob_start();
 
 
 ?>
-<div class="text-center"><img src="view/medias/logos/temp-logo.png" alt="logoLog" class="logoLog"> <br><span class="versiontext"></span>
-<div class="container">
+<div class="text-center"><img src="view/medias/logos/temp-logo.png" alt="logoLog" class="logoLog"> <br><span
+            class="versiontext"></span>
+    <div class="container form-group">
 
-    <h1>Sign In</h1>
-        <form class="" action="index.php?action=signin" method="post">
-            <h6>Informations principales:</h6>
-        <div class="form-row">
+        <h1>Sign In</h1>
+        <form style="align-self: auto" class="" action="index.php?action=signin" method="post">
+            <h5>Informations principales:</h5>
             <div class="">
-                Nom d'utilisateur <input minlength="4" maxlength="20" class="" type="text" name="user" placeholder="Username"  required="required" />
-            </div>
-                <div class= >
-                Prénom <input minlength="2" maxlength="254" type="text" name="name" placeholder="Name"  required="required" />
-            </div>
-            <div class="form-group col-md-6">
-                Nom <input minlength="2" maxlength="254" type="text" name="surname" placeholder="Surname"  required="required" />
-                Initiales <input type="text" name="ini" placeholder="Initials"  required="required" />
-            </div>
-        </div>
-        <div class="form-group">
-            Mot de Passe <input type="password" name="password" placeholder="Password" required="required" />
-            Mot de Passe Confirmation <input type="password" name="passwordc" placeholder="password confirmation" required="required" />
-        </div>
-        <h6>Champs facultatifs:</h6>
-        <div class="form-group">
-            Email <input  class="" type="text" name="email" placeholder="Email"  required="required" />
-            N°télephone <input class="" type="text" name="nb_phone" placeholder="Phone"  required="required" />
-        </div>
-        <div class="form-row">
-            <div class="form-group col-md-6">Biographie <input maxlength="254" class="" type="text" name="bio" placeholder=""  required="required" />
+                <div class=>
+                    Prénom <input minlength="2" maxlength="254" type="text" name="name" placeholder="Name"
+                                  required="required"/>
+                </div>
+                <BR>
+                <div class="">
+                    Nom <input minlength="2" maxlength="254" type="text" name="surname" placeholder="Surname"
+                               required="required"/>
+                </div>
+                <BR>
+                <div class="">
+                    Initiales <input type="text" name="ini" placeholder="Initials" required="required"/>
+                </div>
+                <BR>
+                <h5>Identification:<span title="Inserer le text volu" class="glyphicon glyphicon-question-sign"></span>
+                </h5>
+                <div class="">
+                    Nom d'utilisateur <input minlength="4" maxlength="20" class="" type="text" name="user"
+                                             placeholder="Username" required="required"/>
+                </div>
+                <BR>
+                <div class="">
+                    Mot de Passe <input type="password" name="password" placeholder="Password" required="required"/>
+                </div>
+                <BR>
+                <div class="">
+                    Mot de Passe Confirmation <input type="password" name="passwordc"
+                                                     placeholder="password confirmation" required="required"/> <span
+                            title="Inserer le text volu"
+                            class="glyphicon glyphicon-question-sign"></span>
+                </div>
+                <BR>
+                <h5>Champs facultatifs:</h5>
+                <div class="">
+                    Email <input class="" type="text" name="email" placeholder="Email" required="required"/>
+                </div>
+                <BR>
+                <div class="">
+                    N°télephone <input class="" type="text" name="nb_phone" placeholder="Phone" required="required"/>
+                </div>
+                <BR>
+                <div class="">
+                    <div class="">Biographie <input maxlength="254" class="" type="text" name="bio"
+                                                                       placeholder="" required="required"/>
 
-            <?php
-                if(isset($_SESSION['error']))
-                {
-                    if ($_SESSION['error'] = 1)
-                    {
-                        echo "<br><p class='alert-warning'>Les mots de passe introduits ne se correspondent pas</p>";
-                    }
-                    if ($_SESSION["error"] = 2)
-                    {
-                        echo "<br><p class='alert-warning'>Les initiales introduites sont déjà existantes</p>";
-                    }
+                        <?php
+                        if (isset($_SESSION['error'])) {
+                            if ($_SESSION['error'] = 1) {
+                                echo "<br><p class='alert-warning'>Les mots de passe introduits ne se correspondent pas</p>";
+                            }
+                            if ($_SESSION["error"] = 2) {
+                                echo "<br><p class='alert-warning'>Les initiales introduites sont déjà existantes</p>";
+                            }
 
-                    unset($_SESSION['error']);
-                }
-            ?>
+                            unset($_SESSION['error']);
+                        }
+                        ?>
 
-            </div>
-            <button type="submit" class="btn btn-primary btn-block btn-large">Création du compte</button>
-            <a href="index.php?action=login"><p>Avez vous un compte? Connectez vous</p></a>
-        </div>
-</form>
-
-<?php
-$content = ob_get_clean();
-require "gabarit.php";
-?>
+                    </div>
+                    <BR>
+                    <button type="submit" class="btn btn-primary btn-block ">Création du compte</button>
+                    <a href="index.php?action=login"><p>Avez vous un compte? Connectez vous</p></a>
+                </div>
+        </form>
+    </div>
+    <?php
+    $content = ob_get_clean();
+    require "gabarit.php";
+    ?>
 
 
