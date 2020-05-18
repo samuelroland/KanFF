ed760a2a0846a5216581ebe110a996c2d9b98d1e
KevinVaucherCPNV <47849513+KevinVaucher@users.noreply.github.com>
2020-05-18 15:15:31 +0200

    Create form for the modify account page

--- app/controler/accountControler.php
+++ app/controler/accountControler.php
@@ -1,1 +1,11 @@
 <?php
+/**
+ *  Project: KanFF
+ *  File: accountControler.php
+ *  Author: Kevin Vaucher
+ *  Creation date: 18.05.2020
+ */
+
+function editAccount(){
+    require_once "/view/editAccount.php";
+}
--- app/view/editAccount.php
+++ app/view/editAccount.php
@@ -1,7 +1,43 @@
 <?php
 /**
  *  Project: KanFF
- *  File: localFilesModel.php functions of the model about local files (version.php, settings.json, ...)
+ *  File: edditAccount.php page to allow user to modify his account settings
  *  Author: Kevin Vaucher
  *  Creation date: 18.05.2020
  */
+$title = "KanFF - Modifier le compte";
+ob_start();
+?>
+
+<form>
+    <table>
+        <tbody>
+        <tr>
+            <td><label for="username">Username : </label><input name="username" type="text" id="username"></td>
+        </tr>
+        <tr>
+            <td><label for="firstname">Firstname : </label><input name="firstname" type="text" id="firstname"></td>
+        </tr>
+        <tr>
+            <td><label for="lastname">Lastname : </label><input name="lastname" type="text" id="lastname"></td>
+        </tr>
+        <tr>
+            <td><label for="email">E-mail : </label><input name="email" type="text" id="email"></td>
+        </tr>
+        <tr>
+            <td><label for="phonenumber">Phonenumber : </label><input name="phonenumber" type="text" id="phonenumber"></td>
+        </tr>
+        <tr>
+            <td><label for="bio">Bio : </label><input name="bio" type="text" id="bio"></td>
+        </tr>
+        <tr>
+            <td><label for="password">Password : </label><input name="password" type="text" id="password"></td>
+        </tr>
+        </tbody>
+    </table>
+</form>
+
+<?php
+$content = ob_get_clean();
+require_once 'gabarit.php';
+?>
