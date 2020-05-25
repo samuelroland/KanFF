d7cd4ac0fcd91475c62dd7b1c1a84c23c499173c
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-05-25 09:16:46 +0200

    create home page for dashboard

--- app/view/home.php
+++ app/view/home.php
@@ -1,1 +1,15 @@
 <?php
+/**
+ *  Project: KanFF
+ *  File: home.php firstpage of the website
+ *  Author: Samuel Roland
+ *  Creation date: 25.05.2020
+ */
+$title = "KanFF - Dashboard";
+ob_start();
+?>
+<h1>Dashboard... (en construction)</h1>
+<?php
+$content = ob_get_clean();
+require "gabarit.php";
+?>
