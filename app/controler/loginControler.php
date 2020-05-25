08cc0f877f8a2ae46f31e4118395a9037bc627f8
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-05-25 11:02:58 +0200

    loginControler indent file

--- app/controler/loginControler.php
+++ app/controler/loginControler.php
@@ -12,66 +12,60 @@ require "model/usersModel.php";
 function signin($firstname, $lastname, $initials, $username, $password, $password2, $email, $phoneNumber, $bio)
 {
     // If trying to create an account, else load the page
-    if ($username != "" || $email != "")
-    {
+    if ($username != "" || $email != "") {
         // Checks if password and password2 are equal
-        if ($password == $password2)
-        {
+        if ($password == $password2) {
             // Hash the password for higher security
             $hash = password_hash($password, PASSWORD_DEFAULT);
 
-           /* if (getUser($username) != '')
-            {
-                $_SESSION['error'] = 2;
-                require_once 'view/signin.php';
-            }*/
-           // Saves the user in an array
-           $newUser = [
-               "firstname" => $firstname,
-               "lastname" => $lastname,
-               "initials" => $initials,
-               "username" => $username,
-               "password" => $hash,
-               "email" => $email,
-               "phoneNumber" => $phoneNumber,
-               "bio" => $bio];
+            /* if (getUser($username) != '')
+             {
+                 $_SESSION['error'] = 2;
+                 require_once 'view/signin.php';
+             }*/
+            // Saves the user in an array
+            $newUser = [
+                "firstname" => $firstname,
+                "lastname" => $lastname,
+                "initials" => $initials,
+                "username" => $username,
+                "password" => $hash,
+                "email" => $email,
+                "phoneNumber" => $phoneNumber,
+                "bio" => $bio];
             var_dump($newUser);
             //addUser($newUser);
 
             var_dump($_SESSION);
             require_once "view/login.php";
-        }
-        else{
+        } else {
             $_SESSION['error'] = 2;
             require_once 'view/signin.php';
         }
-    }else{
+    } else {
         require_once 'view/signin.php';
     }
 }
 
 // This funtion will try to Login Using the provided data
-function login($infoLogin,$password)
+function login($infoLogin, $password)
 {
     // TODO: Code the Login function
 
     // If trying to login it checks the data, else load the page
-    if ($infoLogin != "")
-    {
+    if ($infoLogin != "") {
         $UserLog = getUser($infoLogin);
         // Checks if password and password2 are equal
-        if (password_verify($password,$UserLog['password']))
-        {
+        if (password_verify($password, $UserLog['password'])) {
             unset($UserLog['password']);    //unset password to not save it in the session
             $_SESSION['user'] = $UserLog;   //save all informations of the users in the session
             displaydebug($_SESSION);
             require_once 'view/home.php';
-        }
-        else{
+        } else {
             $_SESSION['error'] = 2;
             require_once 'view/login.php';
         }
-    }else{
+    } else {
         require_once 'view/login.php';
     }
 }
