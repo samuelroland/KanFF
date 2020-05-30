2c8443d2c2ad4779d0381652840fd5640670bc90
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-05-30 12:56:11 +0200

    createAGroup.php refactor indent file

--- app/view/createAGroup.php
+++ app/view/createAGroup.php
@@ -36,38 +36,47 @@ $title = "Créer un groupe"
             <div class="col-lg-4 col-md-12 marginauto">
                 <h4>Niveau de visibilité</h4>
                 <select class="fullwidth" name="visibility" id="selVisibility">
-                        <option value="1">Invisible</option>
-                        <option value="2">Titre visible</option>
-                        <option value="3">Image visible</option>
-                        <option value="4">Statut visible</option>
-                        <option value="5">Email visible</option>
-                        <option value="6">Description et contexte visible</option>
-                        <option value="7">Date de création visible</option>
-                        <option value="8">Créateur/Créatrice visible</option>
-                        <option value="9" selected>Membres visibles</option>
-                        <option value="10">Lien du drive visible</option>
-                        <option value="11">Lien du chat visible</option>
-                        <option value="12">Totalement visible</option>
-                    </select></div>
-            <div class="col-lg-8 col-md-12 infotext">Cette option permet de gérer le niveau de visibilité des informations du groupe pour les personnes non-membres. Chaque niveau inclut les précédents. (Image visible inclut que le titre est également visible, par exemple. Et "Email visible" inclut l'email évidemment mais également le statut, l'image et le titre qui seront visibles).
+                    <option value="1">Invisible</option>
+                    <option value="2">Titre visible</option>
+                    <option value="3">Image visible</option>
+                    <option value="4">Statut visible</option>
+                    <option value="5">Email visible</option>
+                    <option value="6">Description et contexte visible</option>
+                    <option value="7">Date de création visible</option>
+                    <option value="8">Créateur/Créatrice visible</option>
+                    <option value="9" selected>Membres visibles</option>
+                    <option value="10">Lien du drive visible</option>
+                    <option value="11">Lien du chat visible</option>
+                    <option value="12">Totalement visible</option>
+                </select></div>
+            <div class="col-lg-8 col-md-12 infotext">Cette option permet de gérer le niveau de visibilité des
+                informations du groupe pour les personnes non-membres. Chaque niveau inclut les précédents. (Image
+                visible inclut que le titre est également visible, par exemple. Et "Email visible" inclut l'email
+                évidemment mais également le statut, l'image et le titre qui seront visibles).
 
-                La visibilité ou au contraire l'invisibilité des projets, travaux, tâches, événements, journaux de bord, ... se gère sur chaque élément en question. (Ainsi un projet peut-être visible alors que d'autres pas).
+                La visibilité ou au contraire l'invisibilité des projets, travaux, tâches, événements, journaux de bord,
+                ... se gère sur chaque élément en question. (Ainsi un projet peut-être visible alors que d'autres pas).
             </div>
         </div>
         <div class="divVisibility row pt-5">
             <div class="col-lg-4 col-md-12 marginauto ">
                 <h4>Type d'accès</h4>
-                <input type="checkbox" id="chkRestrictAccess"><label class="" for="chkRestrictAccess">Accès restreint</label></div>
-            <div class="col-lg-8 col-md-12 infotext">L'accès restreint permet de modérer l'entrée des personnes dans un groupe. Les personnes devront attendre d'être acceptée avant de réelement rejoindre le groupe.
+                <input type="checkbox" id="chkRestrictAccess"><label class="" for="chkRestrictAccess">Accès
+                    restreint</label></div>
+            <div class="col-lg-8 col-md-12 infotext">L'accès restreint permet de modérer l'entrée des personnes dans un
+                groupe. Les personnes devront attendre d'être acceptée avant de réelement rejoindre le groupe.
             </div>
         </div>
-        <p class="mt-5 infotext">Les 2 paramètres "Visible" et "Accès restreint" n'interfèrent pas entre eux. Ils donnent différents niveaux de sécurité des groupes et permettent plus de sécurité ou de confidentialité pour les groupes sensibles.</p>
+        <p class="mt-5 infotext">Les 2 paramètres "Visible" et "Accès restreint" n'interfèrent pas entre eux. Ils
+            donnent différents niveaux de sécurité des groupes et permettent plus de sécurité ou de confidentialité pour
+            les groupes sensibles.</p>
         <div class="divPassword row pt-5">
             <div class="col-lg-4 col-md-12 marginauto ">
                 <h4>Confirmation de l'action</h4>
                 <input class="fullwidth" type="password" name="password" placeholder="Mot de passe">
             </div>
-            <div class="col-lg-8 col-md-12 infotext">Créer un groupe étant une action importante, nous avons besoin de votre confirmation pour valider l'action. Pour ceci rentrer le mot de passe de votre compte.
+            <div class="col-lg-8 col-md-12 infotext">Créer un groupe étant une action importante, nous avons besoin de
+                votre confirmation pour valider l'action. Pour ceci rentrer le mot de passe de votre compte.
             </div>
         </div>
         <div class="divBtnCreate row pt-5">
