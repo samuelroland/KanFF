9cb54f5946fd495db7f607e6a3e82d0c8ffd4596
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-06-03 22:15:20 +0200

    login.php added pt-4 2 times

--- app/view/login.php
+++ app/view/login.php
@@ -7,10 +7,10 @@ ob_start();
     <form class="formlog" action="index.php?action=login" method="post">
         <div>
             <p class="aligncenter">Bienvenue sur l'instance Blason, l'instance KanFF de la GdC pour toute la Romandie.</p>
-            <div class="box-aligncenter"><img src="view/medias/logos/temp-logo.png" alt="logoLog" class="logoLog"> <br><span class="versiontext"></span>
+            <div class="box-aligncenter pt-4"><img src="view/medias/logos/temp-logo.png" alt="logoLog" class="logoLog"> <br><span class="versiontext"></span>
                 <span class="versiontext"></span></div>
         </div>
-        <h1 class="aligncenter">Connexion</h1>
+        <h1 class="aligncenter pt-4">Connexion</h1>
         <div class="form-group">
             <label  for="infoLogin">Nom d'utilisateur, initiales ou email</label>
             <input type="text" width="auto" class="form-control" name="infoLogin" id="infoLogin" autofocus aria-describedby="emailHelp" placeholder="josette27 ou JRD/jrd ou josette.richard@assoc.ch" minlength="3" required>
