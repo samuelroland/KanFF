b1e5eed58df9501180cd4822d2987a62bfe082c7
Luis Pedro Pinheiro <47849666+luispedropinheiro@users.noreply.github.com>
2020-06-05 09:50:46 +0200

    signin

--- app/controler/accountControler.php
+++ app/controler/accountControler.php
@@ -2,7 +2,7 @@
 /**
  *  Project: KanFF
  *  File: accountControler.php
- *  Author: Kevin Vaucher
+ *  Author: Kevin Vaucher et Luís Pedro Pinheiro
  *  Creation date: 18.05.2020
  */
 
@@ -10,3 +10,34 @@ function editAccount(){
     require_once "view/editAccount.php";
 }
 
+//This funtion will redirect to the signin page or redirect to the signin page
+function signin($firstname, $lastname, $initials, $username, $password, $password2, $email, $phoneNumber, $bio)
+{
+    // If trying to create an account, else load the page
+    if ($username != "" || $email != "") {
+        // Checks if password and password2 are equal
+        if ($password == $password2) {
+            // Hash the password for higher security
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+
+            // Saves the user in an array
+            $newUser = [
+                "firstname" => $firstname,
+                "lastname" => $lastname,
+                "initials" => $initials,
+                "username" => $username,
+                "password" => $hash,
+                "email" => $email,
+                "phoneNumber" => $phoneNumber,
+                "bio" => $bio];
+            addUser($newUser);
+
+            require_once "view/login.php";
+        } else {
+            $_SESSION['error'] = 2;
+            require_once 'view/signin.php';
+        }
+    } else {
+        require_once 'view/signin.php';
+    }
+}
--- app/controler/loginControler.php
+++ app/controler/loginControler.php
@@ -8,45 +8,6 @@
 
 require "model/usersModel.php";
 
-//This funtion will redirect to the signin page or redirect to the signin page
-function signin($firstname, $lastname, $initials, $username, $password, $password2, $email, $phoneNumber, $bio)
-{
-    // If trying to create an account, else load the page
-    if ($username != "" || $email != "") {
-        // Checks if password and password2 are equal
-        if ($password == $password2) {
-            // Hash the password for higher security
-            $hash = password_hash($password, PASSWORD_DEFAULT);
-
-            /* if (getUser($username) != '')
-             {
-                 $_SESSION['error'] = 2;
-                 require_once 'view/signin.php';
-             }*/
-            // Saves the user in an array
-            $newUser = [
-                "firstname" => $firstname,
-                "lastname" => $lastname,
-                "initials" => $initials,
-                "username" => $username,
-                "password" => $hash,
-                "email" => $email,
-                "phoneNumber" => $phoneNumber,
-                "bio" => $bio];
-            var_dump($newUser);
-            //addUser($newUser);
-
-            var_dump($_SESSION);
-            require_once "view/login.php";
-        } else {
-            $_SESSION['error'] = 2;
-            require_once 'view/signin.php';
-        }
-    } else {
-        require_once 'view/signin.php';
-    }
-}
-
 // This funtion will try to Login Using the provided data
 function login($infoLogin, $password)
 {
