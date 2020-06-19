e58c68e96f8b37e038ac601adea995f9fa0be3a8
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-06-19 10:00:06 +0200

    version v1.0-beta published
    new versionning system

--- app/version.php
+++ app/version.php
@@ -9,14 +9,23 @@
 //Store the list of the differents versions with a version number, a date of the version and a changelog text about what is new in the app.
 $versions = [
     [
-        "version" => "1.0",
+        "version" => "0.1",
         "date" => "2020-05-11",
         "changelog" => "Start of gabarit, start of the view of signin et create an account. logo temporary."
     ],
     [
-        "version" => "1.1",
+        "version" => "0.2",
         "date" => "2020-05-25",
         "changelog" => "The login is functional (login and logout). The account creation page, login page, account editing page and gabarit are in progress. The logo is temporary."
+    ],
+    [
+        "version" => "1.0-beta",
+        "date" => "2020-06-19",
+        "changelog" => "- KanFF Logo finished! (and favicon created too)\n
+- Continuation of the gabarit with the menu, the design and the colors.\n
+- CRUDModel unit tests in progress.\n
+- Create a group and display the list of the groups, in progress.\n
+- Create an account in progress."
     ]
 ];
 
