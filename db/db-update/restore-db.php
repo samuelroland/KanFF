5ca4d02001813b4a36d3fd60a6b8aa53b7cf525d
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-07-07 22:12:04 +0200

    restore-db.php added comments and fix relative path of .const.php

--- db/db-update/restore-db.php
+++ db/db-update/restore-db.php
@@ -7,17 +7,17 @@
  */
 
 
-require '../.const.php';  //get login informations for db
+require '../../app/.const.php';  //get login informations for db in the app folder
 
 //Drop and create again the database kanff:
-$cmd = "mysql -u $user -p$pass < create-db-kanff.sql";  //system command for execute sql queries or sql file
-exec($cmd);
+$cmdCreate = "mysql -u $user -p$pass < create-db-kanff.sql";  //system command for execute sql queries or sql file
+exec($cmdCreate);
 echo "\n\nDatabase kanff dropped and created again !";
 
 //Fill the db with the data
-$cmd2 = "mysql -u $user -p$pass < fill-data-db-kanff.sql";  //system command for execute sql queries or sql file
+$cmdFill = "mysql -u $user -p$pass < fill-data-db-kanff.sql";  //system command for execute sql queries or sql file
 echo "\n\nDatabase kanff filled with data !";
-exec($cmd2);
+exec($cmdFill);
 
 echo "\n\nEND...";
 ?>
