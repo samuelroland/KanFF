3eb0e038c08a065b871023d7012a4f3a09e560a7
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-08-26 21:17:58 +0200

    gabarit: update comments for a better indentation
    for the planet comment

--- app/view/gabarit.php
+++ app/view/gabarit.php
@@ -31,36 +31,37 @@ $instanceinfos = getInstanceInfos();
 
 </head>
 
-    <!--
-------------------------------------------------
+
+<?php
+//EasterEgg:
+echo "<!-- ----------------------------------------------
             Save together the planet !
 
-              ,,ggddY"""Ybbgg,,
-          ,agd888b,_ "Y8, ___`""Ybga,
-       ,gdP""88888888baa,.""8b    "888g,
-     ,dP"     ]888888888P'  "Y     `888Yb,
-   ,dP"      ,88888888P"  db,       "8P""Yb,
-  ,8"       ,888888888b, d8888a           "8,
- ,8'        d88888888888,88P"' a,          `8,
-,8'         88888888888888PP"  ""           `8,
-d'          I88888888888P"                   `b
-8           `8"88P""Y8P'                      8
-8            Y 8[  _ "                        8
-8              "Y8d8b  "Y a                   8
-8                 `""8d,   __                 8
-Y,                    `"8bd888b,             ,P
+              ,,ggddY\"\"\"Ybbgg,,
+          ,agd888b,_ \"Y8, ___`\"\"Ybga,
+       ,gdP\"\"88888888baa,.\"\"8b    \"888g,
+     ,dP\"     ]888888888P'  \"Y     `888Yb,
+   ,dP\"      ,88888888P\"  db,       \"8P\"\"Yb,
+  ,8\"       ,888888888b, d8888a           \"8,
+ ,8'        d88888888888,88P\"' a,          `8,
+,8'         88888888888888PP\"  \"\"           `8,
+d'          I88888888888P\"                   `b
+8           `8\"88P\"\"Y8P'                      8
+8            Y 8[  _ \"                        8
+8              \"Y8d8b  \"Y a                   8
+8                 `\"\"8d,   __                 8
+Y,                    `\"8bd888b,             ,P
 `8,                     ,d8888888baaa       ,8'
  `8,                    888888888888'      ,8'
-  `8a                   "8888888888I      a8'
+  `8a                   \"8888888888I      a8'
    `Yba                  `Y8888888P'    adP'
-     "Yba                 `888888P'   adY"
-       `"Yba,             d8888P" ,adP"'
-          `"Y8baa,      ,d888P,ad8P"'
-               ``""YYba8888P""''
+     \"Yba                 `888888P'   adY\"
+       `\"Yba,             d8888P\" ,adP\"'
+          `\"Y8baa,      ,d888P,ad8P\"'
+               ``\"\"YYba8888P\"\"''
 
-------------------------------------------------
-
--->
+----------------------------------------------- -->";
+?>
 <body>
 <header class="bg-header header-fixed">
     <div class="logodiv row lineheigthsmall">
@@ -69,8 +70,9 @@ Y,                    `"8bd888b,             ,P
             <br><span class="versiontext">v<?= $versions[count($versions) - 1]['version'] ?></span>
             <span class="versiontext"><em> le <?= date("d.m.Y", strtotime($versions[count($versions) - 1]['date'])) ?></em></span>
         </div>
-        <div data-href="?action=about" class="col-7 collectivename flexdiv clickable <?= ($action == "about") ? 'active' : '' ?>">
-                <div class="align-items-center flexdiv"><?= $instanceinfos['collective']['name'] ?></div>
+        <div data-href="?action=about"
+             class="col-7 collectivename flexdiv clickable <?= ($action == "about") ? 'active' : '' ?>">
+            <div class="align-items-center flexdiv"><?= $instanceinfos['collective']['name'] ?></div>
         </div>
     </div>
 
