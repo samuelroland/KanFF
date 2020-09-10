54f4d00c67fe614610710d2698eb74e4029c622c
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-09-11 01:05:01 +0200

    version v1.1-beta published

--- app/version.php
+++ app/version.php
@@ -28,11 +28,16 @@ $versions = [
 - Create an account in progress."
     ],
     [
-        "version" => "1.1-beta",
-        "date" => "2020-09-10",
-        "changelog" => "- Create an account is possible!
-        - Login is finished (you can login with space at start and end of the username, email or initials)
-        - Design of the gabarit "
+        "version" => "v1.1-beta",
+        "date" => "2020-09-11",
+        "changelog" => "- **Create an account** is possible!
+- **Login** is finished (you can login with space at start and end of the username, email or initials)
+- **Design of the gabarit** is finished too. The user logged can now see his basic information on a dropdown (appear on click on initials) and access to his account or logout. Some of enhancements on the logo position and the version text. The general look of the menu has been updated.
+- **Flashmessages** can now be displayed (in the gabarit or wherever you want in a view).
+- \"**Manage account**\" and \"**Model CRUD**\" are Work In Progress yet.
+- Lots of reflexion have been made on the big feature \"Project management\". Next release will contain project management stories.
+- Work on **technical documentation** have started. Some stories (like Groups page or Create a group) are just on break and not finished unfortunately. This is not our priority now.
+        "
     ]
 ];
 
