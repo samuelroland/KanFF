b5e7eec7acba4c207a87a8ea6f45e2b97ed52ed0
Luis Pedro Pinheiro <47849666+luispedropinheiro@users.noreply.github.com>
2020-10-08 09:10:13 +0200

    FrontEnd of CreateAProjet

--- app/view/createAProject.php
+++ app/view/createAProject.php
@@ -3,19 +3,19 @@ ob_start();
 $title = "Créer un projet"
 ?>
 <h1><?= $title ?></h1>
-<p>Cette page vous permet de créer un nouveau projet réalisé par un groupe dont vous êtes membre</p>
+<p>Cette page vous permet de créer un nouveau projet réalisé par un groupe dont vous êtes membre.</p>
 
 <form action="?action=createAGroup" method="POST" class="pt-4">
     <div class="divContext row pt-4">
         <div class="col-lg-3 col-md-12 ">
             <h4>Nom du Project</h4>
             <input type="text" class="smalltextinput textFieldToCheck" name="name" id="txtName"
-                   maxlength="50" required>
+                   maxlength="70" required>
         </div>
 
-        <div class="col-lg-3 col-md-12 marginauto">
+        <div class="col-lg-3 col-md-10">
             <h4>Importance</h4>
-            <select class="fullwidth" name="group" id="group" required>
+            <select name="group" id="group" required>
                 <?php foreach ($groups as $g) { ?>
                     <option value="<?= $g['id'] ?>"><?= $g['name'] ?></option>
                 <?php } ?>
@@ -23,13 +23,12 @@ $title = "Créer un projet"
         </div>
 
         <div class="col-lg-5 col-md-12 infotext">
-            Notez de 1 à 5 (1 = minimum et 5 = maximum), l'importance de l'urgence du projet.
-            Ce qui permet ensuite de calculer la priorité du projet en privélégiant l'important à l'urgent.
+            Les projets sont réalisés par un ou plusieurs groupes. Vous devez choisir un premier groupe dont vous êtes membre pour créer un projet.
         </div>
     </div>
     <p id="pCounterName">0/50</p>
     <div class="divDescription row pt-4">
-        <div class="col-lg-3 col-md-12"><h4>Description du projet</h4></div>
+        <div class="col-lg-3 col-md-12"><h4>Description</h4></div>
         <div class="col-12"><textarea class="fullwidth textFieldToCheck" name="description" id="txtDescription" rows="2"
                                       maxlength="500" minlength="3" required></textarea>
         </div>
@@ -38,7 +37,7 @@ $title = "Créer un projet"
     <p id="pCounterDescription">0/500</p>
 
     <div class="divContext row pt-4">
-        <div class="col-lg-3 col-md-12"><h4>Objectif du projet</h4></div>
+        <div class="col-lg-3 col-md-12"><h4>Objectif</h4></div>
         <div class="col-12"><textarea class="fullwidth textFieldToCheck" name="goal" id="txtGoal" rows="2"
                                       maxlength="500"
                                       minlength="3"></textarea></div>
@@ -53,7 +52,7 @@ $title = "Créer un projet"
             <input type="date" id="start" name="dateStart" required>
         </div>
         <div class="marginauto">
-            <label for="end">Fin(facultatif):</label>
+            <label for="end">Fin (facultatif):</label>
             <input class="marginauto" type="date" id="end" name="dateEnd">
         </div>
 
@@ -62,11 +61,11 @@ $title = "Créer un projet"
 
     <div class="divImportance row pt-4">
         <div class="col-lg-3 col-md-12"><h3>Priorité du projet</h3></div>
-        <div class="col-lg-8 col-md-12 infotext">
+        <div class="col-md-12 infotext">
             Notez de 1 à 5 (1 = minimum et 5 = maximum), l'importance de l'urgence du projet.
             Ce qui permet ensuite de calculer la priorité du projet en privélégiant l'important à l'urgent.
         </div>
-        <div class="col-lg-4 col-md-12 marginauto">
+        <div class="col-lg-3 col-md">
             <h4>Importance</h4>
 
             <select class="fullwidth" name="importance" id="selImportance" required>
@@ -76,7 +75,7 @@ $title = "Créer un projet"
                 <option value="4">4</option>
                 <option value="5">5</option>
             </select></div>
-        <div class="col-lg-4 col-md-12 marginauto">
+        <div class="col-lg-3 col-md">
             <h4>Urgence</h4>
             <select class="fullwidth" name="urgency" id="selUrgency" required>
                 <option value="1">1</option>
@@ -87,14 +86,17 @@ $title = "Créer un projet"
             </select></div>
     </div>
     <div class="divVisibility row pt-4">
-        <div class="col-lg-4 col-md-12 marginauto">
-            <h4>Type d'accès</h4>
+        <h4>Confidentialité</h4>
+        <div class="col-lg-12 col-md-12">
             <input type="checkbox" id="chkRestrictAccess"
                    name="restrict_access">
-            <label for="chkRestrictAccess">Accès restreint</label>
+            <label for="chkRestrictAccess">Le projet est visible par les personnes non-membres des groupes réalisant ce
+                projet</label>
         </div>
-        <div class="col-lg-8 col-md-12 infotext">L'accès restreint permet de modérer l'entrée des personnes dans un
-            groupe. Les personnes devront attendre d'être acceptée avant de réelement rejoindre le groupe.
+        <div class="col-lg-12 col-md-12">
+            <input type="checkbox" id="chkRestrictAccess"
+                   name="restrict_access">
+            <label for="chkRestrictAccess">Le journal de bord est visible par les personnes non-membres des groupes réalisant ce projet</label>
         </div>
     </div>
     <p class="mt-5 infotext">Les 2 paramètres "Visible" et "Accès restreint" n'interfèrent pas entre eux. Ils
