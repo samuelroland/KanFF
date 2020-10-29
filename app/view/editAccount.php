5b34f9c20014620c53a7b393028e57ab43a1afd7
SOARES Miguel <Miguel.SOARES@cpnv.ch>
2020-10-29 09:33:01 +0100

    icon change + instance function

--- app/view/editAccount.php
+++ app/view/editAccount.php
@@ -10,8 +10,9 @@ $cssForSpan = "col-md-5 col-sm-5 box-verticalaligncenter spanForForm";
 $cssForInput = "col-md-5 col-sm-7 form-control inputForForm";
 $cssForDivZone = "pl-3";
 $cssForDivField = "row pt-1";
-
 $title = "Mon compte";
+$instanceinfos = getInstanceInfos();
+$instance = $instanceinfos['instance'];
 ob_start();
 ?>
     <div class="<?= $cssForDivZone ?>">
@@ -19,7 +20,7 @@ ob_start();
         <div class="flexdiv">
             <h1 class="flex-1"><?= $title ?></h1>
             <div class="flex-1 d-block">
-                <button class="btn btn-primary float-right">Détails de cette instance blason</button>
+                <button class="btn btn-primary float-right">Détails de cette instance <?=$instance['name']  ?></button>
             </div>
         </div>
         <p class="">Voici les informations de votre compte sur l'instance Blason. C'est sur cette page que vous
@@ -50,15 +51,14 @@ ob_start();
                         <span class="<?= $cssForSpan ?>">Initiales </span>
                         <input class="<?= $cssForInput ?>" type="text" value="<?= $user['initials'] ?>" readonly
                                disabled/>
-                        <img title="Les initiales sont uniques et générées automatiquement donc non modifiables"
-                             src="view/medias/icons/point.png" alt="question sign" width="35" height="35" class="">
+                        <?= createToolTip(printAnIcon("point.png", "", "question mark icon", "icon-small ml-2 mr-2 m-2", false), "Les initiales sont uniques et générées automatiquement donc non modifiables", false) ?>
                     </div>
                     <div class="<?= $cssForDivField ?>">
                         <span class="<?= $cssForSpan ?>">Date d'inscription </span>
                         <input class="<?= $cssForInput ?>" type="date"
                                value="<?= date("Y-m-d", strtotime($user['inscription'])) ?>" readonly disabled/>
-                        <img title="Date d'inscription non modifiable"
-                             src="view/medias/icons/point.png" alt="question sign" width="35" height="35" class="">
+                        <?= createToolTip(printAnIcon("point.png", "", "question mark icon", "icon-small ml-2 mr-2 m-2", false), "Date d'inscription non modifiable", false) ?>
+
                     </div>
 
                     <div class="<?= $cssForDivField ?>">
@@ -88,8 +88,8 @@ ob_start();
                         <span class=" <?= $cssForSpan ?>">Etat du compte </span>
                         <input class="<?= $cssForInput ?>" type="text" readonly disabled
                                value="<?= convertUserState($user['state']) ?>"/>
-                        <img title="Cet état peut être non aprouvé, aprouvé, archivé ou admin"
-                             src="view/medias/icons/point.png" alt="question sign" width="35" height="35" class="">
+                        <?= createToolTip(printAnIcon("point.png", "", "question mark icon", "icon-small ml-2 mr-2 m-2", false), "Cet état peut être non aprouvé, aprouvé, archivé ou admin", false) ?>
+
                     </div>
                     <div class="<?= $cssForDivField ?>">
                         <span class=" <?= $cssForSpan ?>">Changement d'état</span>
@@ -107,17 +107,18 @@ ob_start();
                             }
 
                             ?></span>
-
-                        <img title="Cet état peut être non aprouvé, aprouvé, archivé ou admin"
-                             src="view/medias/icons/point.png" alt="question sign" width="35" height="35" class="">
+                        <div  class="">
+                        <?= createToolTip(printAnIcon("point.png", "", "question mark icon", "icon-small ml-2 mr-2 m-2", false), "Cet état peut être non aprouvé, aprouvé, archivé ou admin", false) ?>
+                        </div>
                     </div>
 
                     <div class="<?= $cssForDivField ?>">
                         <span class=" <?= $cssForSpan ?>">En pause</span>
                         <input class="<?= $cssForInput ?> " type="checkbox" id="inpOnBreak"
                                readonly disabled <?= ($user['on_break'] == 1) ? "checked" : "" ?>/>
-                        <img title="La valeur 'En pause' défini moralement que votre engagement dans ce collectif est en pause. La seule différence est que vous apparaîtrez dans la liste des membres sous l'option 'En pause' au lieu de l'option 'Actif'."
-                             src="view/medias/icons/point.png" alt="question sign" width="35" height="35" class="">
+
+                        <?= createToolTip(printAnIcon("point.png", "", "question mark icon", "icon-small ml-2 mr-2 m-2", false), "La valeur 'En pause' défini moralement que votre engagement dans ce collectif est en pause. La seule différence est que vous apparaîtrez dans la liste des membres sous l'option 'En pause' au lieu de l'option 'Actif'.", false) ?>
+
                     </div>
 
                 <h5 class="pt-3">Champs facultatifs:</h5>
@@ -138,9 +139,9 @@ ob_start();
                         <span class="<?= $cssForSpan ?>">Lien messagerie instantanée</span>
                         <input class="<?= $cssForInput ?>" type="email" name="email"
                                placeholder="t.me/josette27" value="<?= $user['chat_link'] ?>"/>
-                        <img title="Lien publique contenant votre pseudo publique. Fonctionne pour certaines messageries uniquement.
-Ex: pseudo = jeanrichard alors sur Telegram: t.me/jeanrichard"
-                             src="view/medias/icons/point.png" alt="question sign" width="35" height="35" class="">
+                        <?= createToolTip(printAnIcon("point.png", "", "question mark icon", "icon-small ml-2 mr-2 m-2", false), "Lien publique contenant votre pseudo publique. Fonctionne pour certaines messageries uniquement.
+Ex: pseudo = jeanrichard alors sur Telegram: t.me/jeanrichard", false) ?>
+
                     </div>
 
 
@@ -175,10 +176,10 @@ Ex: pseudo = jeanrichard alors sur Telegram: t.me/jeanrichard"
             <div class= "<?= $cssForDivField ?>">
                 <span class="<?= $cssForSpan ?>">nouveau mot de passe</span>
                 <input class="<?= $cssForInput ?>" type="password" name="newpassword" placeholder="" required/>
-                <img title="Les critères de sécurité du mot de passe sont:
+                <?= createToolTip(printAnIcon("point.png", "", "question mark icon", "icon-small ml-2 mr-2 m-2", false), "Les critères de sécurité du mot de passe sont:
                 - yy caractères
                 - caractères minuscules, majuscules, spéciaux, chiffres.
-                - ... TBD" src="view/medias/icons/point.png" alt="question sign" width="35" height="35" class="">
+                - ... TBD", false) ?>
             </div>
 
 
