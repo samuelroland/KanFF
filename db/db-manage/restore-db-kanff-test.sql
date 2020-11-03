533e06e8f740f1fd7d5528ab65496b18730fcad7
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-11-03 22:46:49 +0100

    repository: move models, deleted 2 old files, moved 2 files of doc or reflexion

--- db/db-manage/restore-db-kanff-test.sql
+++
@@ -1,9 +0,0 @@
-/**
- *  Project: KanFF
- *  File: restore-db-kanff-test.sql SQL script for restore the database and its data, if database isn't created or data has been changed.
- *  Author:
- *  Creation date:
- */
- /*
- Change Cartouche !!!
- */
