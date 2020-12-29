dfa370a16c744f8abcc527c1b93afaf85598ab2a
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-12-29 14:59:53 +0100

    login: information displayed when a beta version is installed

--- app/view/login.php
+++ app/view/login.php
@@ -3,11 +3,16 @@ ob_start();
 $title = "Connexion";
 $instanceinfos = getInstanceInfos();
 $instance = $instanceinfos['instance'];
+$versions = getVersionsApp();
+$lastVersion = $versions[count($versions) - 1];
 ?>
 
     <form class="formlog" action="?action=login" method="post">
         <div>
             <p class="aligncenter"><?= $instanceinfos['collective']['msg'] ?></p>
+            <?php if (contains($lastVersion['version'], "beta") == true) {
+                echo "<p class='text-primary statebanner'><strong>Une version bêta est installée. Cette version est donc instable, contient des bugs et des fonctionnalités à moitié implémentée... Elle ne doit pas être utilisée en conditions réelles (que dans un but de tests).</strong></p>";
+            } ?>
             <?php if ($instance['testinstance'] == true) {
                 echo "<p class='text-danger'><strong>Vous êtes sur une instance de test et les données sont fictives. Elles doivent le rester car elles sont accessibles publiquement! (Ne créez pas de compte, projet, travail, tâche, groupe, ... réel).</strong></p>";
             } ?>
@@ -32,7 +37,9 @@ $instance = $instanceinfos['instance'];
                 <button type="submit" class="btn btn-primary mt-2">Se connecter</button>
             </div>
             <br>
-            <p>Pas encore de compte sur cette instance ? <a href="?action=signin">Créer un compte.</a></p>
+            <p class="flex-1">Pas encore de compte sur cette instance ? <a href="?action=signin">Créer un compte.</a>
+            </p>
+            <?php //<p class="alignright"><a href="link" target="_blank">En savoir plus sur KanFF.</a></p> ?>
         </div>
 
     </form>
