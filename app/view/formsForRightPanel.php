ca4c319395981183ff75fa28691d9877bcbe8cea
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-12-31 00:14:40 +0100

    kanban: task details: open link and copy link buttons have been replaced by icons

--- app/view/formsForRightPanel.php
+++ app/view/formsForRightPanel.php
@@ -92,9 +92,9 @@ function printDivTaskDetails($project)
                 </div>
                 <div class="<?= $divCSS ?>">
                     <div class="flexdiv">
-                        <span class="flex-1">Lien:</span>
-                        <span class="alignright" <?= getInlineJSForALinkToCopy("link.value", true) ?>>Copier le lien</span>
-                        <span class="alignright" <?= getInlineJSForALinkToOpen("link.value", true, "_blank") ?>>Ouvrir le lien</span>
+                        <span class="flex-1 align-items-end">Lien:</span>
+                        <?= createToolTip('<span class="alignright yellowdarkonhover d-inline-block" ' . getInlineJSForALinkToCopy("link.value", true) . '>' . printAnIcon("copylink.png", "", "copy link icon", "icon-middlesmall m-2", false) . '</span>', "Copier le lien"); ?>
+                        <?= createToolTip('<span class="alignright yellowdarkonhover d-inline-block" ' . getInlineJSForALinkToOpen("link.value", true) . '>' . printAnIcon("openlink.png", "", "open link icon", "icon-middlesmall m-2", false) . '</span>', "Ouvrir le lien dans un nouvel onglet"); ?>
                     </div>
                     <input type="text" placeholder="Lien relatif à la tâche" name="link" id="link"
                            class="form-control textFieldToCheck counterVisibleOnlyIfFastMaxLength inputspan"
