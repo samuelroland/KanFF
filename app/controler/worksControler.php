7d35187cf3caa7d00660afbdb405faa1110b1f11
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2021-01-22 21:50:11 +0100

    worksControler.php: hasWritingRightOnTasksOfAWork() return false if $work is invalid (false or empty)

--- app/controler/worksControler.php
+++ app/controler/worksControler.php
@@ -22,6 +22,10 @@ function createAWork($data)
 //Return true or false, if the user has writing right on tasks of a work depending on the settings of the work
 function hasWritingRightOnTasksOfAWork($isInTheProject, $work)
 {
+    //if work is empty or false, the query to get it from the db has probably failed, so the authorization cannot be given.
+    if (empty($work) || $work == false) {
+        return false;
+    }
     $open = $work['open'];
     $visible = $work['visible'];
     if (isAtLeastEqual($work['state'], [WORK_STATE_DONE, WORK_STATE_TODO]) == false) {
